91b646398ffd62160f0c9015a6d4b4d905dd4a92
clivemchd <clivemchd@gmail.com>
2017-08-06 18:41:09 +0530

    uploaded photo to facebook using graph api

--- app.js
+++ app.js
@@ -10,11 +10,31 @@ var app 				= express()
 var request			= require('request')
 
 /**
- * Server listneing on port 7000
- * @param 	INT 7000 		[Port Number]
+ * Server listneing on port 5005
+ * @param 	INT 5005 		[Port Number]
  * @param 	Callback    [Consoles the port at which Server is created]
  */
-app.listen(4045, () => {
-	console.log('\x1b[36m','Server listening on port','\x1b[1m', '\x1b[37m', '4045', '\x1b[0m')
-	console.log(' Server Open at : ', '\x1b[33m', '\x1b[1m', 'http://localhost:4045', '\x1b[0m')
+app.listen(5005, () => {
+	console.log('\x1b[36m','Server listening on port','\x1b[1m', '\x1b[37m', '5005', '\x1b[0m')
+	console.log(' Server Open at : ', '\x1b[33m', '\x1b[1m', 'http://localhost:5005', '\x1b[0m')
 })
+
+app.get('/', function(req, res){
+	var dataString = 
+	'url=http://i.qkme.me/3ofxwf.jpg&access_token=EAACEdEose0cBAFd2cxC8iXZAo0MkqPo0aMmyHU1We8bv9dn6WEL5it8hE8ld8ZAldhSrAXstlZBCeVEDiRCzZChZBSZAVIKrdGFvq1d6aCR1ZCCdrH2Nwdpu5bz8xjKBO5dfTl1KlZAbua1MIG3cXwEK3bFaLwbexCUms1DFyPDOKUgtwoD1eIRxCrMs1uoDUYIZD';
+
+	var options = {
+			url: 'https://graph.facebook.com/v2.4/me/photos',
+	    method: 'POST',
+	    body: dataString
+	}
+
+	request(options, function(error, response, body){
+		 if (!error && response.statusCode == 200) {
+				console.log(body);
+	    }
+	    res.end()
+	})
+})
+
+
